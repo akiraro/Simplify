1bc9b175977a5e60ea4a009a596f5c9507ebc4e2
HAFIZI <haziq_yusof@hotmail.com>
2023-06-12 18:53:25 -0400

    Change slug API URL

--- hooks/useSimplify.ts
+++ hooks/useSimplify.ts
@@ -2,7 +2,7 @@ import useSWR from "swr";
 import fetcher from "@/lib/fetcher";
 
 const useSimplify = (slug?: any) => {
-	const { data, error, isLoading } = useSWR(slug ? `/api/url/${slug}`: null, fetcher, {
+	const { data, error, isLoading } = useSWR(slug ? `/api/public/${slug}`: null, fetcher, {
 		revalidateIfStale: false,
 		revalidateOnFocus: false,
 		revalidateOnReconnect: false,
